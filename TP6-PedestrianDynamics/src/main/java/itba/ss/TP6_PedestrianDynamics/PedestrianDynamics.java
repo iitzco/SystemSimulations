46fa05b5f12777e721c87fdf2e05b6ad843e43df
Ivan Itzcovich <i.itzcovich@gmail.com>
2016-10-31 10:12:35 -0300

    Add Pedestrian Dynamics codebase

---
+++ TP6-PedestrianDynamics/src/main/java/itba/ss/TP6_PedestrianDynamics/PedestrianDynamics.java
@@ -0,0 +1,9 @@
+package itba.ss.TP6_PedestrianDynamics;
+
+public class PedestrianDynamics {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
